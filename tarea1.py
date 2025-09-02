f625aad08daf2fc20702b2f3ddeba4465f556e04
angiemesaca <angmesaca@unal.edu.co>
2025-09-02 13:21:53 -0500

    quinta version tarea1.py

--- tarea1.py
+++ tarea1.py
@@ -3,10 +3,10 @@
 # encontrar los numeros primos entre 0  y 50 
 
 for numero in range (2,51):
-    primo = T
+    primo = True
     for division in range (2, numero):
         if numero % division == 0 :
-            primo = F
-
-if primo == T:
-    print (numero)
+            primo = False
+            break
+    if primo == True:
+        print (numero)
